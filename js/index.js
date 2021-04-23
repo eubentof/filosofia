e5cd2293b06f2e780bcc3bce9a80086f28ccd045
Bento <dev.bento@gmail.com>
2021-04-23 17:10:17 -0300

    initial

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.font = "30px Arial";
+ctx.textAlign = "center";
+ctx.fillText("UMA BREVE HISTÓRIA DO TEMPO", 10, 50);
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
